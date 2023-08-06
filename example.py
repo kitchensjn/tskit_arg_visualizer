5265d6fe63967592b92590dc9ed116140a404dc9
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2023-08-05 18:30:37 -0700

    setup.py installation

--- example.py
+++ example.py
@@ -1,8 +1,7 @@
 import msprime
-import tskit
 import random
 import json
-from visualizer import visualizer
+import tskit_arg_visualizer
 
 # Generate a random tree sequence with record_full_arg=True so that you get marked recombination nodes
 ts_rs = random.randint(0,10000)   
@@ -33,7 +32,7 @@ print(ts_rs)
 
 #ts = tskit.load("/Users/jameskitchens/Documents/GitHub/sparg2.0/ARGweaver/slim/condensed.trees")
 
-d3arg = visualizer.D3ARG(ts=ts)
+d3arg = tskit_arg_visualizer.D3ARG(ts=ts)
 d3arg.draw(width=1000, height=750, y_axis_labels=True, y_axis_scale="log_time", tree_highlighting=True, edge_type="line")
 
 
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="tskit_arg_visualizer",
+    version="0.1",
+    url='https://github.com/kitchensjn/tskit_arg_visualizer',
+    author="James Kitchens",
+    author_email="kitchensjn@gmail.com",
+    packages=["tskit_arg_visualizer"],
+    install_requires=[
+        "msprime",
+        "IPython",
+    ],
+)
