c3a185cc1485b95e29e469f654f73939534b7d5f
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2023-08-06 09:15:23 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tskit_arg_visualizer",
-    version="0.1",
+    version="0.10",
     url='https://github.com/kitchensjn/tskit_arg_visualizer',
     author="James Kitchens",
     author_email="kitchensjn@gmail.com",
