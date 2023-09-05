cd90c1531835a1bd169afbdf206f7cc6a68e5239
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2023-09-05 15:01:49 -0700

    Dynamic vnub in edge
    `vnub` the part of the edge that comes in from above a bifurcation node or below a recombination node. Previously, the `vnub` was fixed at 20px and if the nodes were too close together vertically, it would lead to an ugly overlap. Now `vnub` is set to either 20 or the half the vertical distance between the two nodes, whichever is smaller.

--- example.py
+++ example.py
@@ -33,7 +33,7 @@ print(ts_rs)
 #ts = tskit.load("/Users/jameskitchens/Documents/GitHub/sparg2.0/ARGweaver/slim/condensed.trees")
 
 d3arg = tskit_arg_visualizer.D3ARG(ts=ts)
-d3arg.draw(width=500, height=500, edge_type="line")
+d3arg.draw(width=500, height=500, edge_type="ortho")
 
 
 # Or draw from a previously saved tree sequence which is stored in a JSON file
