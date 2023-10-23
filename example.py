cdd6e5ce7cb63811f1ffd21819707b82e215c9a9
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2023-10-22 17:45:19 -0700

    prepare for release
    Final edits to prepare for release of v0.0.1-alpha

--- example.py
+++ example.py
@@ -13,29 +13,11 @@ ts = msprime.sim_ancestry(
     record_full_arg=True,
     random_seed=5139
 )
-print(5139)
-
-#mts_rs = random.randint(1,10000)
-#mts = msprime.sim_mutations(
-#    tree_sequence=ts, 
-#    rate=2.5e-8,
-#    random_seed=mts_rs
-#)
-
-#print("ts random seed:", ts_rs)
-#print("mts random seed:", mts_rs)
-
-#print(mts.tables.mutations)
-
-#print(mts.first().draw_text())
-
-
-#ts = tskit.load("/Users/jameskitchens/Documents/GitHub/sparg2.0/ARGweaver/slim/condensed.trees")
 
 d3arg = tskit_arg_visualizer.D3ARG(ts=ts)
 d3arg.draw(width=500, height=500, edge_type="line", variable_edge_width=True)
 
 
 # Or draw from a previously saved tree sequence which is stored in a JSON file
-#arg_json = json.load(open("example3.json", "r"))
+#arg_json = json.load(open("example.json", "r"))
 #tskit_arg_visualizer.draw_D3(arg_json=arg_json)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tskit_arg_visualizer",
-    version="0.10",
+    version="v0.0.1-alpha",
     url='https://github.com/kitchensjn/tskit_arg_visualizer',
     author="James Kitchens",
     author_email="kitchensjn@gmail.com",
