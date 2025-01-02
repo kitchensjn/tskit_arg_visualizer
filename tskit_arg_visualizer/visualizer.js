a45f32592cb140bb779445b3cd27f1b266bd2ddf
James Kitchens <40303683+kitchensjn@users.noreply.github.com>
2025-01-02 09:16:45 -0500

    potential solve to #116
    I was unable to recreate @hyanwong's issue, but I think that's because I had already fixed it and hadn't committed to GitHub. His example code works with this commit.

--- tskit_arg_visualizer/visualizer.js
+++ tskit_arg_visualizer/visualizer.js
@@ -222,7 +222,7 @@ require(["d3"], function(d3) {
             .attr("d", "M153.6 29.9l16-21.3C173.6 3.2 180 0 186.7 0C198.4 0 208 9.6 208 21.3V43.5c0 13.1 5.4 25.7 14.9 34.7L307.6 159C356.4 205.6 384 270.2 384 337.7C384 434 306 512 209.7 512H192C86 512 0 426 0 320v-3.8c0-48.8 19.4-95.6 53.9-130.1l3.5-3.5c4.2-4.2 10-6.6 16-6.6C85.9 176 96 186.1 96 198.6V288c0 35.3 28.7 64 64 64s64-28.7 64-64v-3.9c0-18-7.2-35.3-19.9-48l-38.6-38.6c-24-24-37.5-56.7-37.5-90.7c0-27.7 9-54.8 25.6-76.9z");
         reheat.append("span").attr("class", "tip desc").text("Reheat Simulation");
         
-        if ($plot_type == "full") {
+        if ("$plot_type" == "full") {
             var evenly_distribute = dashboard.append("button").attr("class", "dashbutton activecolor")
                 .on("click", function() {
                     var order = d3.selectAll("#arg_${divnum} .flag1").data().sort((a, b) => d3.ascending(a.x, b.x)).map(a => a.id);;
